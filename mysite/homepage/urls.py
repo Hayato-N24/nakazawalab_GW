2a3b220bf1f1a2940887883d63895e636b2f4f0d
GAO-ZX <57781000+GAO-ZX@users.noreply.github.com>
2021-06-30 13:52:58 +0900

    usersデータ取り出し

--- mysite/homepage/urls.py
+++ mysite/homepage/urls.py
@@ -3,5 +3,5 @@ from . import views
 from django.urls import include, path
 
 urlpatterns = [
-    path('templates/', views.index, name='index'),
-]
+    path('', views.list_view, name='list_view'),
+    ]
--- mysite/homepage/views.py
+++ mysite/homepage/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import MySQLdb
 
 # Create your views here.
-def index(request):
-    return render(request, 'homepage.html')
+def list_view(request):
+    conn = MySQLdb.connect(
+        user="nakazawalab",
+        password="hogehoge",
+        host="localhost",
+        db="app_data"
+    )
+
+    # カーソルの取得
+    cur = conn.cursor()
+
+    # SQL実行
+    # selfIntroductionテーブルから全レコード取り出す
+    sql = "SELECT * from users"
+    cur.execute(sql)
+
+    # 実行結果の取得
+    rows = cur.fetchall()
+    cur.close
+    conn.close
+
+    return render(request, 'homepage.html',context={'data':rows})
