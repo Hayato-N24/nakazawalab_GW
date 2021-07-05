cfc20430085d3d5b664a7072f1266ddfa95dc9d4
Hayato-N24 <0409shido@gmail.com>
2021-07-05 21:29:51 +0900

    almost done

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class UserInfo(models.Model):
-    userName = models.CharField(max_length=200)
+    userName = models.CharField(max_length=200,unique=True)
     allergy = models.CharField(max_length=200, null=True)
     like = models.CharField(max_length=200, null=True)
     dislike = models.CharField(max_length=200, null=True)
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -35,6 +35,9 @@ def addUser(request):
         userForm = UserForm(request.POST)
         if userForm.is_valid():
             userForm.save()
+        else:
+            errorhtml = '<a href="create">戻る</a>'
+            return HttpResponse('そのユーザーネームは既に使用されています。' + errorhtml)
  
     #登録後、全件データを抽出
     userinfo = UserInfo.objects.all()
@@ -73,13 +76,16 @@ def searchResult(request):
 
     keyword = request.GET.get('keyword')
 
-    if keyword:
+    if keyword is not None:
        
         model = model.filter(
             userName__iexact = keyword
         )
-        if model.count() > 1:
+        if model.count() > 0:
             messages.success(request, "「{}」の検索結果".format(keyword))
+        else:
+            errorhtml = '<a href="searchForm">戻る</a>'
+            return HttpResponse('該当のユーザーは存在しません' + errorhtml)
         
     
     return render(request, 'myapp/searchResult.html', {'result':model})
