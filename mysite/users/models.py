b9b04a67757d32d75bb6b6c6c19a529ab2e0cae8
Hayato-N24 <0409shido@gmail.com>
2021-07-07 18:51:50 +0900

    done

--- mysite/users/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class UserInfo(models.Model):
-    username= models.CharField(max_length=100)
-    mailaddress= models.EmailField()
-    selfIntroduction= models.CharField(max_length=200)
-    allergie= models.CharField(max_length=200)
-
